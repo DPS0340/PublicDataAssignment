dec8c0ad65eb28104fbb155b47f1cff2866b2dfd
jiho lee <optional.int@kakao.com>
2020-11-14 19:30:28 +0900

    Add: Loading screen

--- app/src/main/java/com/example/publicdataassignment/MainActivity.java
+++ app/src/main/java/com/example/publicdataassignment/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.publicdataassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("MAIN", "Showing loading screen..");
+        Activity activity = this;
+        ProgressBar progressBar = findViewById(R.id.loadingProgressBar);
+        progressBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent destIntent = new Intent(activity, ShowActivity.class);
+                startActivity(destIntent);
+            }
+        });
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        Log.i("MAIN", "Loading is done!");
+                        goShowActivity();
+                    }
+                },
+                (int)(0.8 * 1000));
+    }
+
+    private void goShowActivity() {
+        Intent destIntent = new Intent(this, ShowActivity.class);
+        startActivity(destIntent);
     }
 }
