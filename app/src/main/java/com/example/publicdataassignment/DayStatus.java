b726d9cdf5eaea63110aed9ab7911c861151b247
jiho lee <optional.int@kakao.com>
2020-11-14 23:02:20 +0900

    Add: show xml

---
+++ app/src/main/java/com/example/publicdataassignment/DayStatus.java
@@ -0,0 +1,8 @@
+package com.example.publicdataassignment;
+
+public abstract class DayStatus {
+    public static int SATISFIED = 1,
+            DISSATISFIED = 2,
+            VERY_SATISFIED = 3,
+            VERY_DISSATISFIED = 4;
+}
--- app/src/main/java/com/example/publicdataassignment/SelectActivity.java
+++ app/src/main/java/com/example/publicdataassignment/SelectActivity.java
@@ -89,8 +89,8 @@ public class SelectActivity extends AppCompatActivity {
                     Toast.makeText(activity, "동 이름을 입력해 주세요.", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                goShowActivity(dong);
                 dialog.dismiss();
+                goShowActivity(dong);
             }
         });
         dialog.show();
